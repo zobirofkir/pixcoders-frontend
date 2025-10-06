28410c40cb0ac2f31d693331e8701e53d3a302df
csw <zobirofkir19@gmail.com>
2025-10-06 23:34:54 +0100

    fix erorr

--- src/animations/variants.js
+++ src/animations/variants.js
@@ -52,3 +52,15 @@ export const scaleUp = {
     }
   }
 };
+
+export const fadeInUp = {
+  hidden: { opacity: 0, y: 40 },
+  show: {
+    opacity: 1,
+    y: 0,
+    transition: {
+      duration: 0.6,
+      ease: [0.6, -0.05, 0.01, 0.99]
+    }
+  }
+};
