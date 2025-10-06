da8b3fda63bb173d0d9868d3894330e54d3920cf
csw <zobirofkir19@gmail.com>
2025-10-06 23:25:31 +0100

    clean

--- src/data/skillsData.js
+++ src/data/skillsData.js
@@ -1,10 +1,12 @@
 export const skillsData = [
   { name: 'Web Development', icon: '🌐' },
+  { name: 'App Development', icon: '📲' },
   { name: 'UI/UX Design', icon: '🎨' },
-  { name: 'Mobile Development', icon: '📱' },
   { name: 'Cloud Architecture', icon: '☁️' },
   { name: 'DevOps', icon: '🔄' },
   { name: 'AI/ML Solutions', icon: '🤖' },
   { name: 'Blockchain', icon: '⛓️' },
   { name: 'Data Analytics', icon: '📊' },
+  { name: 'Photography', icon: '📸' },
+  { name: 'Image & Video Editing', icon: '🎬' },
 ];
