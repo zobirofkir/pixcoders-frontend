8c9a7d198178c70c3deb09bea2649521ba2a40b1
csw <zobirofkir19@gmail.com>
2025-10-06 22:27:24 +0100

    fix error

--- app/users/abouts/page.js
+++ app/users/abouts/page.js
@@ -1,5 +1,5 @@
-import AboutComponent from '@/src/components/users/about/AboutComponent'
-import LoadingComponent from '@/src/components/loading/LoadingComponent'
+import AboutComponent from '@/components/users/about/AboutComponent'
+import LoadingComponent from '@/components/loading/LoadingComponent'
 import React, { Suspense } from 'react'
 
 const page = () => {
