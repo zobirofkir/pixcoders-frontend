f531766deadbca3c82de70b14c7ce2d145d64f7e
csw <zobirofkir19@gmail.com>
2025-10-06 23:03:46 +0100

    clear import

--- src/components/users/about/AboutComponent.js
+++ src/components/users/about/AboutComponent.js
@@ -2,7 +2,7 @@
 
 import React from 'react';
 import { motion } from 'framer-motion';
-import { HeroSection, AboutSection, StatsSection, BackgroundElements } from './components';
+import { AboutHeroSectionComponent, AboutAboutSectionComponent, AboutStatsSectionComponent, AboutBackgroundElementsComponent } from '@/components';
 
 const AboutComponent = () => {
   return (
@@ -12,12 +12,12 @@ const AboutComponent = () => {
       viewport={{ once: true, amount: 0.1 }}
       className="relative py-24 overflow-hidden bg-gradient-to-b from-white to-gray-50 dark:from-gray-900 dark:to-gray-950"
     >
-      <BackgroundElements />
+      <AboutBackgroundElementsComponent />
       
       <div className="relative max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-        <HeroSection />
-        <AboutSection />
-        <StatsSection />
+        <AboutHeroSectionComponent />
+        <AboutAboutSectionComponent />
+        <AboutStatsSectionComponent />
       </div>
     </motion.section>
   );
