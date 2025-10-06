b03c76bfb1d4afea7901da139adb1c700289a9db
csw <zobirofkir19@gmail.com>
2025-10-06 01:06:41 +0100

    remove

--- app/layout.js
+++ app/layout.js
@@ -1,17 +1,12 @@
 import './globals.css';
 import { metadata } from '../src/MetaData/Metadata';
-import ThemeScript from '../src/components/layout/ThemeScriptComponent';
-import Body from '../src/components/layout/BodyComponent';
 
 export { metadata };
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en" className="scroll-smooth" suppressHydrationWarning>
-      <head>
-        <ThemeScript />
-      </head>
-      <Body>{children}</Body>
+    <html lang="en" className="scroll-smooth">
+      <body>{children}</body>
     </html>
   );
 }
