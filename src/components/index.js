b9c773830df243c9af651a56b3cbd741a9dc9600
csw <zobirofkir19@gmail.com>
2025-10-06 14:21:26 +0100

    add talents and works import

--- src/components/index.js
+++ src/components/index.js
@@ -1,9 +1,15 @@
+/**
+ * Talents Import
+ */
 export { default as WorkCardComponent } from './users/works/WorkCardComponent';
 export { default as FilterButtonsComponent } from './users/works/FilterButtonsComponent';
 export { default as HeroSectionComponent } from './users/works/HeroSectionComponent';
 export { default as CTASectionComponent } from './users/works/CTASectionComponent';
 export { default as WorksGridComponent } from './users/works/WorksGridComponent';
 
+/**
+ * Works Import
+ */
 export { default as FilterSectionComponent } from './users/talent/filters/FilterSectionComponent';
 export { default as SearchFilterComponent } from './users/talent/filters/SearchFilterComponent';
 export { default as SkillsFilterComponent } from './users/talent/filters/SkillsFilterComponent';
