c6f9ee14a60b8ca4e1588351444ca07f0d1e8714
csw <zobirofkir19@gmail.com>
2025-10-06 15:29:17 +0100

    add

--- src/components/contact/ContactComponent.js
+++ src/components/contact/ContactComponent.js
@@ -69,7 +69,7 @@ const ContactComponent = () => {
   ];
 
   return (
-    <Suspense fallback={LoadingComponent}>
+    <Suspense fallback={<LoadingComponent/>}>
 
       <section id="contact" className="py-20 bg-gray-50 dark:bg-gray-900 overflow-hidden">
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
