f27e7814f56f75d0ceeca7862e2487957daec16c
csw <zobirofkir19@gmail.com>
2025-10-06 09:45:57 +0100

    add header and footer components in layout

--- app/layout.js
+++ app/layout.js
@@ -1,11 +1,20 @@
 import './globals.css';
 import { metadata } from '../src/MetaData/Metadata';
+import HeaderComponent from '@/public/images/slider/nav/HeaderComponent';
+import FooterComponent from '@/public/images/slider/nav/FooterComponent';
 
 export { metadata };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en" className="scroll-smooth">
+      <header>
+        <HeaderComponent />
+      </header>
+
+      <footer>
+        <FooterComponent />
+      </footer>
       <body>{children}</body>
     </html>
   );
