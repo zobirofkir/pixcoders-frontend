b60165fee98cd1c7efa79e54f89ff5c7a2bc2815
csw <zobirofkir19@gmail.com>
2025-10-06 23:15:17 +0100

    fix

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['picsum.photos'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'picsum.photos',
+      },
+    ],
   },
 };
 
