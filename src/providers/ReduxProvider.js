0dc720d1aeaee108c5ee184acc03b45a941d467e
csw <zobirofkir19@gmail.com>
2025-10-07 20:49:34 +0100

    remove

--- src/providers/ReduxProvider.js
+++ src/providers/ReduxProvider.js
@@ -1,8 +1,12 @@
 'use client';
 
 import { Provider } from 'react-redux';
-import store from '@/redux/store/store';
+import { store } from '@/redux/store/store';
 
 export default function ReduxProvider({ children }) {
-  return <Provider store={store}>{children}</Provider>;
+  return (
+    <Provider store={store}>
+      {children}
+    </Provider>
+  );
 }
--- src/redux/store/store.js
+++ src/redux/store/store.js
@@ -1,28 +1,11 @@
-import { configureStore } from '@reduxjs/toolkit';
-import { persistStore, persistReducer } from 'redux-persist';
-import storage from 'redux-persist/lib/storage';
+import { configureStore, combineReducers } from '@reduxjs/toolkit';
 import authReducer from '../slices/authSlice';
 
-// Configuration for redux-persist
-const persistConfig = {
-  key: 'root',
-  storage,
-  whitelist: ['auth'], // Only persist the auth state
-};
-
-// Create a persisted reducer
-const persistedReducer = persistReducer(persistConfig, {
+const rootReducer = combineReducers({
   auth: authReducer,
 });
 
 export const store = configureStore({
-  reducer: persistedReducer,
-  middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware({
-      serializableCheck: {
-        ignoredActions: ['persist/PERSIST', 'persist/REHYDRATE'],
-      },
-    }),
+  reducer: rootReducer,
+  devTools: process.env.NODE_ENV !== 'production',
 });
-
-export const persistor = persistStore(store);
