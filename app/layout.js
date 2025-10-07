56e04cd0f6b86a8ce73f54c96c7721372e9624a7
csw <zobirofkir19@gmail.com>
2025-10-07 20:46:12 +0100

    add provider

--- app/layout.js
+++ app/layout.js
@@ -2,6 +2,7 @@ import './globals.css';
 import { metadata } from '../src/MetaData/Metadata';
 import HeaderComponent from '@/src/components/nav/HeaderComponent';
 import FooterComponent from '@/src/components/nav/FooterComponent';
+import ReduxProvider from '@/src/providers/ReduxProvider';
 
 export { metadata };
 
@@ -9,13 +10,11 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en" className="scroll-smooth">
       <body>
-
-        <HeaderComponent />
-
-        {children}
-
-        <FooterComponent />
-        
+        <ReduxProvider>
+          <HeaderComponent />
+          {children}
+          <FooterComponent />
+        </ReduxProvider>
       </body>
     </html>
   );
