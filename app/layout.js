12666d424cf9e9cb2f8eee76925d9829716e93ac
csw <zobirofkir19@gmail.com>
2025-10-08 06:45:53 +0100

    clean

--- app/layout.js
+++ app/layout.js
@@ -1,8 +1,11 @@
 import './globals.css';
 import { metadata } from '../src/MetaData/Metadata';
+import dynamic from 'next/dynamic';
 import HeaderComponent from '@/src/components/nav/HeaderComponent';
+import AuthHeaderComponent from '@/src/components/nav/auth/AuthHeaderComponent';
 import FooterComponent from '@/src/components/nav/FooterComponent';
 import ReduxProvider from '@/src/providers/ReduxProvider';
+import { isAuthenticated } from '@/src/utils/cookies';
 
 export { metadata };
 
@@ -11,7 +14,7 @@ export default function RootLayout({ children }) {
     <html lang="en" className="scroll-smooth">
       <body>
         <ReduxProvider>
-          <HeaderComponent />
+          {isAuthenticated() ? <AuthHeaderComponent /> : <HeaderComponent />}
           {children}
           <FooterComponent />
         </ReduxProvider>
